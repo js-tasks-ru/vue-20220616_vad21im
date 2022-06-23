3d0c4d0929639bb8e2e73f88e9659d26d57ea4bf
Vadim <Vad21im@mail.ru>
2022-06-23 17:42:52 +0300

    4ая задача готова
    Третей займусь по позже) пора пить чай:)

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,8 +1,9 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp} from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
 function fetchMeetupById(meetupId) {
+  console.log(meetupId)
   return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
     if (response.ok) {
       return response.json();
@@ -14,4 +15,22 @@ function fetchMeetupById(meetupId) {
   });
 }
 
+createApp({
+  data() {
+    return {
+      changeRadio: 0,
+      titleMeetup: ''
+    }
+  },
+  watch: {
+    changeRadio: {
+      async handler() {
+        if (this.changeRadio > 0 && this.changeRadio < 6) {
+          let res = await fetchMeetupById(this.changeRadio)
+          this.titleMeetup = res.title
+        }
+      }
+    }
+  }
+}).mount('#app')
 // Требуется создать Vue приложение
