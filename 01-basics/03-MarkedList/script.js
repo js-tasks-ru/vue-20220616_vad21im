02e5fa8720541e0ba17777b1d1c943300a3fa5eb
Vadim <Vad21im@mail.ru>
2022-06-30 23:31:35 +0300

    Готово
    Подсмотрел решение

--- 01-basics/03-MarkedList/script.js
+++ 01-basics/03-MarkedList/script.js
@@ -1,45 +1,49 @@
 import {createApp} from './vendor/vue.esm-browser.js';
+const emails=  [
+  'Eliseo@gardner.biz',
+  'Jayne_Kuhic@sydney.com',
+  'Nikita@garfield.biz',
+  'Lew@alysha.tv',
+  'Hayden@althea.biz',
+  'Presley.Mueller@myrl.com',
+  'Dallas@ole.me',
+  'Mallory_Kunze@marie.org',
+  'Meghan_Littel@rene.us',
+  'Carmen_Keeling@caroline.name',
+  'Veronica_Goodwin@timmothy.net',
+  'Oswald.Vandervort@leanne.org',
+  'Kariane@jadyn.tv',
+  'Nathan@solon.io',
+  'Maynard.Hodkiewicz@roberta.com',
+  'Christine@ayana.info',
+  'Preston_Hudson@blaise.tv',
+  'Vincenza_Klocko@albertha.name',
+  'Madelynn.Gorczany@darion.biz',
+  'Mariana_Orn@preston.org',
+  'Noemie@marques.me',
+  'Khalil@emile.co.uk',
+  'Sophia@arianna.co.uk',
+  'Jeffery@juwan.us',
+  'Isaias_Kuhic@jarrett.net',
+];
 
-// Создайте Vue приложение
 createApp({
   data() {
     return {
-      emails: [
-        'Eliseo@gardner.biz',
-        'Jayne_Kuhic@sydney.com',
-        'Nikita@garfield.biz',
-        'Lew@alysha.tv',
-        'Hayden@althea.biz',
-        'Presley.Mueller@myrl.com',
-        'Dallas@ole.me',
-        'Mallory_Kunze@marie.org',
-        'Meghan_Littel@rene.us',
-        'Carmen_Keeling@caroline.name',
-        'Veronica_Goodwin@timmothy.net',
-        'Oswald.Vandervort@leanne.org',
-        'Kariane@jadyn.tv',
-        'Nathan@solon.io',
-        'Maynard.Hodkiewicz@roberta.com',
-        'Christine@ayana.info',
-        'Preston_Hudson@blaise.tv',
-        'Vincenza_Klocko@albertha.name',
-        'Madelynn.Gorczany@darion.biz',
-        'Mariana_Orn@preston.org',
-        'Noemie@marques.me',
-        'Khalil@emile.co.uk',
-        'Sophia@arianna.co.uk',
-        'Jeffery@juwan.us',
-        'Isaias_Kuhic@jarrett.net',
-      ],
-      search: ''
-    }
+      emails,
+      filter: '',
+    };
   },
-  computed(){
 
-  }
-}).mount('#app')
-
-
+  computed: {
+    markedEmails() {
+      return this.emails.map((email) => ({
+        email,
+        marked: this.filter && email.includes(this.filter),
+      }));
+    },
+  },
+}).mount('#app');
 
 
 // Требуется создать Vue приложение
